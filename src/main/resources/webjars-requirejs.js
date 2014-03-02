dfa83f8bd73b5a95e43c2c0ba22889b92b094a33
Patrick Kaeding <patrick@kaeding.name>
2014-03-02 09:24:48 -0800

    removed popypasta cruft

--- src/main/resources/webjars-requirejs.js
+++ src/main/resources/webjars-requirejs.js
@@ -1,17 +1,3 @@
-var webjarsRequirejsPluginsChildren = [
-    'async',
-    'font',
-    'goog',
-    'image',
-    'json',
-    'mdown',
-    'noext',
-    'propertyParser'
-];
-var webjarsRequirejsPluginsPaths = {};
-webjarsRequirejsPluginsChildren.forEach(function (child) {
-    webjarsRequirejsPluginsPaths[child] = webjars.path("requirejs-plugins", child);
-});
 requirejs.config({
     paths: {
         "mustache": webjars.path("mustachejs", "mustache")
